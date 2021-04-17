6f79d75d0d086c5d2a49931ab677ec5a14aab918
Luis Felipe Murguia Ramos <felipelfmr1@gmail.com>
2021-04-17 15:34:24 -0700

    Initial create

---
+++ ui/utils/utils.dart
@@ -0,0 +1,16 @@
+//EXPORT FROM DATA
+export '../../data/repositories/repositories.dart';
+export '../../data/services/services.dart';
+
+//EXPORT FROM DOMAIN
+export '../../domain/controllers/controllers.dart';
+export '../../domain/models/models.dart';
+export '../../domain/repositories/repositories.dart';
+export '../../domain/services/services.dart';
+export '../../domain/utils/utils.dart';
+
+//EXPORT FROM UI
+export '../widgets/widgets.dart';
+export 'constants.dart';
+export 'enums.dart';
+export 'theme/theme.dart';
